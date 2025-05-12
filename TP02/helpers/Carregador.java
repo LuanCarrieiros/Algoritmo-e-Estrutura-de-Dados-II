57fdbc6789ef926221de56f83afbf050a5c4b247
Luan Carrieiros <yluan.s70@hotmail.com>
2025-05-12 18:15:56 -0300

    Update Carregador.java

--- TP02/helpers/Carregador.java
+++ TP02/helpers/Carregador.java
@@ -4,7 +4,6 @@ import java.io.*;
 import java.nio.charset.Charset;
 import java.util.*;
 
-
 public class Carregador {
     public static List<Personagem> carregar(String caminho) throws IOException {
         List<Personagem> list = new ArrayList<>();
