ca6d98cef5e0d3f4749c7b57c4f0d11a70f02054
Luan Carrieiros <yluan.s70@hotmail.com>
2025-05-12 18:13:57 -0300

    Create Q11_SortByYear.java

---
+++ TP02/Q11_SortByYear.java
@@ -0,0 +1,33 @@
+import helpers.Carregador;
+import helpers.Personagem;
+import helpers.SortUtils;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q11_SortByYear {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        List<Personagem> personagens = Carregador.carregar("tmp/characters.csv");
+
+        // Ler IDs até "FIM"
+        List<Personagem> subset = new java.util.ArrayList<>();
+        String line;
+        while (!(line = sc.nextLine()).equals("FIM")) {
+            for (Personagem p : personagens) {
+                if (p.getId().equals(line)) {
+                    subset.add(p);
+                    break;
+                }
+            }
+        }
+
+        // Ordena por ano de nascimento usando Counting Sort
+        SortUtils.countingSort(subset);
+
+        // Imprime todos os personagens ordenados
+        subset.forEach(Personagem::imprimir);
+        sc.close();
+    }
+}
