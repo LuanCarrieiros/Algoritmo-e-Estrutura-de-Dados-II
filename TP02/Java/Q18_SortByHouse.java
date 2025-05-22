fe4564bf040cbfbd11681ceed2b7157762ac2272
Luan Carrieiros <yluan.s70@hotmail.com>
2025-05-21 22:32:50 -0300

    Create Q18_SortByHouse.java

---
+++ TP02/Java/Q18_SortByHouse.java
@@ -0,0 +1,33 @@
+package Java;
+import Java.helpers.Carregador;
+import Java.helpers.Personagem;
+import Java.helpers.SortUtils;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Q18_SortByHouse {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        List<Personagem> personagens = Carregador.carregar("java/tmp/characters.csv");
+
+        // Ler IDs até "FIM"
+        List<Personagem> subset = new java.util.ArrayList<>();
+        String line;
+        while (!(line = sc.nextLine()).equals("FIM")) {
+            for (Personagem p : personagens) {
+                if (p.getId().equals(line)) {
+                    subset.add(p);
+                    break;
+                }
+            }
+        }
+
+        // Ordena por casa completo (quicksort parcial com k=size)
+        SortUtils.quickSortPartial(subset, subset.size());
+
+        // Imprime ordenado
+        subset.forEach(Personagem::imprimir);
+        sc.close();
+    }
+}
