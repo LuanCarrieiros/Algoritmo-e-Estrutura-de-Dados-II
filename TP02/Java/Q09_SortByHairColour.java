1df9919473290c08c40e04dcd1b367f6df5632d8
Luan Carrieiros <yluan.s70@hotmail.com>
2025-05-21 22:32:34 -0300

    Create Q09_SortByHairColour.java

---
+++ TP02/Java/Q09_SortByHairColour.java
@@ -0,0 +1,28 @@
+package Java;
+import Java.helpers.Carregador;
+import Java.helpers.Personagem;
+import java.io.IOException;
+import java.util.List;
+import java.util.Scanner;
+import Java.helpers.SortUtils;
+
+public class Q09_SortByHairColour {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        List<Personagem> personagens = Carregador.carregar("java/tmp/characters.csv");
+
+        List<Personagem> subset = new java.util.ArrayList<>();
+        String line;
+        while (!(line = sc.nextLine()).equals("FIM")) {
+            for (Personagem p : personagens) {
+                if (p.getId().equals(line)) {
+                    subset.add(p);
+                    break;
+                }
+            }
+        }
+        SortUtils.heapSort(subset);
+        subset.forEach(Personagem::imprimir);
+        sc.close();
+    }
+}
