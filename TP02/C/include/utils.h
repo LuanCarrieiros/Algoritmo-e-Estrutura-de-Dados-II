3363be3d19a5a5f97ef90655dd7c2973a93faf62
Luan Carrieiros <yluan.s70@hotmail.com>
2025-05-23 09:57:20 -0300

    Create utils.h

---
+++ TP02/C/include/utils.h
@@ -0,0 +1,29 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include "../include/common.h" // Para String, StringArray, bool
+#include "../include/list.h"   // Para List
+#include "../include/personagem.h" // Para Personagem (usada em read)
+
+// --------------------------------------- MANIPULAÇÃO DE STRINGS ---------------------------------- //
+
+bool equals(String str1, String str2);
+bool isBigger(String str1, String str2);
+bool isShorter(String str1, String str2);
+void isYes(bool value);
+void format_Date(String date);
+int countDelim(char delim, String str);
+StringArray split(String str, char delimitador);
+String replace(String str, char oldChar, char newChar);
+String trim(String str);
+void remove_char(String str, char removee);
+
+// Funções para carregar e obter fragmentos de personagens
+ListPersonagem createList(String FileName);
+Personagem* getFragmentsOfCharacter(ListPersonagem *List, int *size);
+StringArray fillFieldWithIds(int *size);
+void showFragmentsOfCharacter(Personagem *personagem, int size);
+void showFragmentsOfCharacter_static(StringArray personagem, int size, ListPersonagem *List);
+
+
+#endif
