97f2f895674a3d9c31b2e0d5f96599fe4ae1f6dc
Luan Carrieiros <yluan.s70@hotmail.com>
2025-05-25 00:45:19 -0300

    Delete search_algos.h

--- TP02/C/include/search_algos.h
+++
@@ -1,9 +0,0 @@
-#ifndef SEARCH_ALGOS_H
-#define SEARCH_ALGOS_H
-
-#include "../include/common.h"      // Para StringArray, Log
-#include "../include/linked_list.h" // Para ListPersonagem
-
-bool binarySearch (StringArray armazenamentoIDs, String searched, int left, int right, ListPersonagem *List, Log *log);
-
-#endif
