78a67183db3f5ec297ab72cf3e4e3f58deac42bb
Luan Carrieiros <yluan.s70@hotmail.com>
2025-08-30 21:30:03 -0300

    Update Q01_Palindromo.java

--- TP01/Q01_Palindromo.java
+++ TP01/Q01_Palindromo.java
@@ -3,15 +3,11 @@ import java.util.*;
 public class Q01_Palindromo {
     public static void main(String[] args) 
     {   
-       
         Scanner scanf = new Scanner(System.in);
-        // leitura da entrada
         String str = scanf.nextLine();
         
-        // loop para continuar recebendo strings ate que a palavra "FIM" seja digitada, é feito um while ao inves de um do while para evitar que o programa imprima mais uma linha, informando se é palindromo ou nao, após digitar "FIM"
         while (!str.equals("FIM"))
         {   
-            // condicional para testar se o resultado foi verdadeiro ou falso
             if(isPalindrome(str))
             {
                 System.out.println("SIM");
@@ -19,13 +15,17 @@ public class Q01_Palindromo {
             {
                 System.out.println("NAO");
             }
-            // releitura para continuar analisando novas entradas
             str = scanf.nextLine();
         }
+        
         scanf.close();
     }
 
-    // método booleano que retorna verdadeiro ou falso após analisar a string que recebeu por parametro 
+    /**
+     * Verifica se uma string é palíndromo
+     * @param str string a ser verificada
+     * @return true se for palíndromo, false caso contrário
+     */ 
     public static boolean isPalindrome (String str) 
     {
         int length = str.length();
