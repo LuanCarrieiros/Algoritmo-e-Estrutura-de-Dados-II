25103778bedce222d62178f9d59718da2f3b11dd
Luan Carrieiros <yluan.s70@hotmail.com>
2025-08-30 21:34:09 -0300

    Update Q12_CifraCesarRec.java

--- TP01/Q12_CifraCesarRec.java
+++ TP01/Q12_CifraCesarRec.java
@@ -2,7 +2,13 @@ import java.util.*;
 
 public class Q12_CifraCesarRec 
 {
-    // Método para criptografar uma mensagem usando a cifra de César
+    /**
+     * Método para criptografar uma mensagem usando a cifra de César de forma recursiva
+     * @param str string original
+     * @param chave deslocamento a ser aplicado
+     * @param index índice atual para recursão
+     * @return string cifrada
+     */
     public static String criptografar(String str, int chave, int index) {
         
         String cifrado = "";
@@ -15,23 +21,27 @@ public class Q12_CifraCesarRec
         char caractere = str.charAt(index); // pegando caracter posicao por posicao, recursivamente
         int codigo = (int) caractere; // casting de char para int
     
-            // Verifica se o caractere é um caractere imprimível da tabela ASCII
-            if (codigo >= 32 && codigo <= 126) {
-                // Aplica a cifra de César para o caractere
-                codigo = (codigo - 32 + chave) % 95 + 32;
-                cifrado += (char) codigo;
-            } else {
-                // Caso o caractere não seja um caractere imprimível, mantém o mesmo
-                cifrado += caractere;
-            }
+        // Verifica se o caractere é um caractere imprimível da tabela ASCII
+        if (codigo >= 32 && codigo <= 126) {
+            // Aplica a cifra de César para o caractere
+            codigo = (codigo - 32 + chave) % 95 + 32;
+            cifrado += (char) codigo;
+        } else {
+            // Caso o caractere não seja um caractere imprimível, mantém o mesmo
+            cifrado += caractere;
+        }
 
         // Concatena o caractere cifrado com o resultado da criptografia do restante da string
         return cifrado + criptografar(str, chave, index + 1);
     }
 
+    /**
+     * Verifica se a string é "FIM"
+     * @param text string a ser verificada
+     * @return true se for "FIM", false caso contrário
+     */
     public static boolean isEnd(String text) 
     {
-        // Verifica se o texto tem comprimento 3 e os caracteres são 'F', 'I', 'M'
         return text.length() == 3 && text.charAt(0) == 'F' && text.charAt(1) == 'I' && text.charAt(2) == 'M';
     }
 
